6930079de38f60b9414046dd672c5c84e1e04e8e
nakamuuu-prog <affilimoney2@gmail.com>
2024-10-09 09:09:01 +0900

    Productクラスの警告を解消

--- Models/Product.cs
+++ Models/Product.cs
@@ -6,22 +6,22 @@ namespace UdemyReservationAppServer.Models
   {
     public int Id { get; set; }
 
-    public string CoverImage { get; set; }
+    public string? CoverImage { get; set; }
 
     [Required(ErrorMessage = "Name is required")]
     [MaxLength(60, ErrorMessage = "最大60文字までです！")]
-    public string Name { get; set; }
+    public string Name { get; set; } = string.Empty;
 
     public decimal Price { get; set; }
 
-    public string Description { get; set; }
+    public string? Description { get; set; }
 
-    public string Heading1 { get; set; }
-    public string Heading2 { get; set; }
-    public string Heading3 { get; set; }
+    public string? Heading1 { get; set; }
+    public string? Heading2 { get; set; }
+    public string? Heading3 { get; set; }
 
-    public string HeadingText1 { get; set; }
-    public string HeadingText2 { get; set; }
-    public string HeadingText3 { get; set; }
+    public string? HeadingText1 { get; set; }
+    public string? HeadingText2 { get; set; }
+    public string? HeadingText3 { get; set; }
   }
 }
